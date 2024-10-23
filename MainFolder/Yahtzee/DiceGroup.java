c69845e99eaf6c406476a257040e7d6bb18b0f32
harlyliu <99936708+harlyliu@users.noreply.github.com>
2024-10-23 10:11:48 -0700

    Add files via upload

---
+++ MainFolder/Yahtzee/DiceGroup.java
@@ -0,0 +1,117 @@
+public class DiceGroup {
+	
+	private Dice [] die;	// the array of dice
+	
+	private final int NUM_DICE = 5;	// number of dice
+	
+	// Create the seven different line images of a die
+	private String [] line = {	" _______ ",
+								"|       |",
+								"| O   O |",
+								"|   O   |",
+								"|     O |",
+								"| O     |",
+								"|_______|" };
+	
+	/*	you complete */
+	public DiceGroup() { 
+		die = new Dice[6];
+		for (int i = 0; i < NUM_DIEC; i++){
+			die[i] = new Dice();
+		}
+	}
+	
+	/**	you complete */
+	public void rollDice() { 
+		for (int i = 0;i < NUM_DICE; i++)die[i].roll();
+	}
+	
+	/**	Hold the dice in the rawHold and roll the rest.
+	 *	For example: If rawHold is "421", then hold die 1, 2, and 4, and
+	 *	roll 3 and 5.
+	 *	@param rawHold		the string of dice to hold
+	 *
+	 *	you complete
+	 */
+	public void rollDice(String rawHold) {
+		for (int i = 0; i < NUM_DICE; i++){
+			boolean valid = true;
+			for (int j = 0; j < rawHold.length(); j++){
+				if (Integer.parseInt(rawHold.charAt(j))-1 == i) valid = false;
+			}
+			if (valid) die[i].roll();
+		}
+	}
+	
+	/**	getters - you complete */
+	public Dice getDice(int i) return die[i];
+	
+	/**	@return the total value of the DiceGroup - you complete */
+	public int getTotal() {
+		int ans = 0;
+		for (int i = 0; i < numDice; i++){
+			ans += numDice[i].getValue();
+		}
+		return ans;
+	}
+	
+	/**
+	 *  Prints out the images of the dice
+	 */
+	public void printDice() {
+		printDiceHeaders();
+		for (int i = 0; i < 6; i++) {
+			System.out.print(" ");
+			for (int j = 0; j < die.length; j++) {
+				printDiceLine(die[j].getValue() + 6 * i);
+				System.out.print("     ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
+	/**
+	 *  Prints the first line of the dice.
+	 */
+	private void printDiceHeaders() {
+		System.out.println();
+		for (int i = 1; i < 6; i++) {
+			System.out.printf("   # %d        ", i);
+		}
+		System.out.println();
+	}
+	
+	/**
+	 *  Prints one line of the ASCII image of the dice.
+	 *
+	 *  @param value The index into the ASCII image of the dice.
+	 */
+	private void printDiceLine(int value) {
+		System.out.print(line[getDiceLine(value)]);
+	}
+	
+	/**
+	 *  Gets the index number into the ASCII image of the dice.
+	 *
+	 *  @param value The index into the ASCII image of the dice.
+	 */
+	private int getDiceLine(int value) {
+		if (value < 7) return 0;
+		if (value < 14) return 1;
+		switch (value) {
+			case 20: case 22: case 25:
+				return 1;
+			case 16: case 17: case 18: case 24: case 28: case 29: case 30:
+				return 2;
+			case 19: case 21: case 23:
+				return 3;
+			case 14: case 15:
+				return 4;
+			case 26: case 27:
+				return 5;
+			default:	// value > 30
+				return 6;
+		}
+	}
+}
---
+++ MainFolder/Yahtzee/Yahtzee.java
@@ -0,0 +1,97 @@
+public class Yahtzee{
+	final int TOTAL_TURNS = 13;
+	YahtzeePlayer player1;
+	YahtzeePlayer player2;
+	DiceGroup diceGroup1;
+	DiceGroup diceGroup2;
+	
+	public Yahtzee(){}
+	
+	public static void main(String[] args){
+		Yahtzee yahtzee = new Yahtzee();
+		yahtzee.playGame();
+	}
+	
+	public void playGame(){
+		printHeader();
+		player1 = new YahtzeePlayer();
+		player2 = new YahtzeePlayer();
+		String player1Name = Prompt.getString("Player 1, please enter your first name ");
+		String player2Name = prompt.getString("Player 2, please enter your first name ");
+		player1.setName(player1Name);
+		player2.setName(player2Name);
+		diceGroup1 = new DiceGroup();
+		diceGroup2 = new DiceGroup();
+		int first = determineOrder();
+		for (int i = 0; i < TOTAL_TURNS; i++){
+			if (first == 1){
+				runPlayerTurn(player1);
+				runPlayerTurn(player2);
+			}
+			else{
+				runPlayerTurn(player2);
+				runPlayerTurn(player1);
+			}
+			player1.getScoreCard().printCardHeader();
+			player1.getScoreCard().printPlayerScore();
+			player2.getScoreCard().printPlayerScore();
+		}
+	}
+	
+	public void runPlayerTurn(int player){
+		String inp;
+		if (player == 1){
+			do{
+				Prompt.getString(player1.getName + " , it's your turn to play. Please hit enter to roll the dice");
+				DiceGroup1.rollDice();
+				DiceGroup1.printDice();
+				inp = Prompt.getString("Which di(c)e would you like to keep? "
+				+"Enter the values you'd like to 'hold' without"
+				+ "spaces. For examples, if you'd like to 'hold' die 1, "
+				+ "2, and 5, enter 125 (enter -1 if you'd like to end the turn)"); 
+			}while(!inp.equals("-1"));
+			
+		}
+		
+	}
+	
+	public int determineOrder(){
+		int val1 = 0;
+		int val2 = 0;
+		do{
+			Prompt.getString("Let's see who will go first. " + player1.getName() + 
+				", please hit enter to roll the dice");
+			diceGroup1.rollDice();
+			val1 = diceGroup1.getTotal();
+			Prompt.getString(player2.getName() + 
+				", it's your turn. Please hit enter to roll the dice");
+			diceGroup2.rollDice();
+			val2 = diceGroup2.getTotal();
+		}while(val1 != val2);
+		if (val1 > val2) return 1;
+		return 2;
+	}
+	
+	
+	public void printHeader() {
+		System.out.println("\n");
+		System.out.println("+------------------------------------------------------------------------------------+");
+		System.out.println("| WELCOME TO MONTA VISTA YAHTZEE!                                                    |");
+		System.out.println("|                                                                                    |");
+		System.out.println("| There are 13 rounds in a game of Yahtzee. In each turn, a player can roll his/her  |");
+		System.out.println("| dice up to 3 times in order to get the desired combination. On the first roll, the |");
+		System.out.println("| player rolls all five of the dice at once. On the second and third rolls, the      |");
+		System.out.println("| player can roll any number of dice he/she wants to, including none or all of them, |");
+		System.out.println("| trying to get a good combination.                                                  |");
+		System.out.println("| The player can choose whether he/she wants to roll once, twice or three times in   |");
+		System.out.println("| each turn. After the three rolls in a turn, the player must put his/her score down |");
+		System.out.println("| on the scorecard, under any one of the thirteen categories. The score that the     |");
+		System.out.println("| player finally gets for that turn depends on the category/box that he/she chooses  |");
+		System.out.println("| and the combination that he/she got by rolling the dice. But once a box is chosen  |");
+		System.out.println("| on the score card, it can't be chosen again.                                       |");
+		System.out.println("|                                                                                    |");
+		System.out.println("| LET'S PLAY SOME YAHTZEE!                                                           |");
+		System.out.println("+------------------------------------------------------------------------------------+");
+		System.out.println("\n\n");
+	}
+}
