9be57b47455885ee2bffe439c9226592a2122ad9
harlyliu <99936708+harlyliu@users.noreply.github.com>
2024-11-01 10:04:34 -0700

    Add files via upload

---
+++ HTMLUtilities/HTMLUtilities.java
@@ -0,0 +1,40 @@
+/**
+ *	Utilities for handling HTML
+ *
+ *	@author	
+ *	@since	
+ */
+public class HTMLUtilities {
+
+	/**
+	 *	Break the HTML string into tokens. The array returned is
+	 *	exactly the size of the number of tokens in the HTML string.
+	 *	Example:	HTML string = "Goodnight moon goodnight stars"
+	 *				returns { "Goodnight", "moon", "goodnight", "stars" }
+	 *	@param str			the HTML string
+	 *	@return				the String array of tokens
+	 */
+	public String[] tokenizeHTMLString(String str) {
+		// make the size of the array large to start
+		String[] result = new String[10000];
+		
+		// return the correctly sized array
+		return result;
+	}
+	
+	/**
+	 *	Print the tokens in the array to the screen
+	 *	Precondition: All elements in the array are valid String objects.
+	 *				(no nulls)
+	 *	@param tokens		an array of String tokens
+	 */
+	public void printTokens(String[] tokens) {
+		if (tokens == null) return;
+		for (int a = 0; a < tokens.length; a++) {
+			if (a % 5 == 0) System.out.print("\n  ");
+			System.out.print("[token " + a + "]: " + tokens[a] + " ");
+		}
+		System.out.println();
+	}
+
+}
