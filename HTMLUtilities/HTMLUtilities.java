9bb5ca2b432dc14c236de8a392d70f6229882e20
harlyliu <99936708+harlyliu@users.noreply.github.com>
2024-11-13 09:50:55 -0800

    Update HTMLUtilities.java

--- HTMLUtilities/HTMLUtilities.java
+++ HTMLUtilities/HTMLUtilities.java
@@ -5,7 +5,11 @@
  *	@since	
  */
 public class HTMLUtilities {
-
+	// NONE = not nested in a block, COMMENT = inside a comment block
+	// PREFORMAT = inside a pre-format block
+	private enum TokenState { NONE, COMMENT, PREFORMAT };
+	// the current tokenizer state
+	private TokenState state = TokenState.NONE;
 	/**
 	 *	Break the HTML string into tokens. The array returned is
 	 *	exactly the size of the number of tokens in the HTML string.
@@ -20,78 +24,102 @@ public class HTMLUtilities {
 		
 		String currStr = new String();
 		boolean inTag = false;
+		if (str.equals("<pre>")){
+			state = TokenState.PREFORMAT;
+			tokens[0] = str;
+		}
+		else if (state == TokenState.PREFORMAT && str.equals("</pre>")){
+			state = TokenState.NONE;
+			tokens[0] = str;
+		}
+		else if (state == TokenState.PREFORMAT){
+			tokens[0] = str;
+		}
 		
 		for (int i = 0; i < str.length(); i++) {
 		    char c = str.charAt(i);
-		
-		    if (c == '<') {
-		        if (currStr.length() > 0) {
-		            tokens[count++] = currStr.toString();
-		            currStr = "";
-		        }
-		        inTag = true;
-		        currStr += c;
-		    }
-		    else if (c == '>') {
-		        if (inTag) {
-		            currStr += c;
-		            tokens[count++] = currStr;
-		            currStr = "";
-		            inTag = false;
-		        }
-		    }
-		    else if (inTag) {
-		        currStr += c;
-		    }
-		    else if (Character.isWhitespace(c)) {
-		        if (currStr.length() > 0) {
-		            tokens[count++] = currStr.toString();
-		            currStr = "";
-		        }
-		    }
-		    else if (Character.isDigit(c) || c == '.') {
-		        if (currStr.length() > 0 && 
-		            !Character.isDigit(currStr.charAt(currStr.length() - 1)) &&
-		            currStr.charAt(currStr.length() - 1) != '.' &&
-		            !(currStr.length() == 1 && currStr.charAt(0) == '-')) {
-		            tokens[count++] = currStr;
-		            currStr = "";
-		        }
-		        currStr += c;
-		    }
-		    else if (c == '-') {
-		        if (currStr.length() == 0 || 
-		            (!Character.isDigit(currStr.charAt(0)) && !Character.isLetter(currStr.charAt(0)))) {
-		            currStr += c;
-		        }
-				else if (currStr.length() > 0 && Character.isLetter(currStr.charAt(currStr.length() - 1))) {
+			if (i +4 <= str.length() && str.substring(i, i+4).equals("<!--")){
+				state = TokenState.COMMENT;
+				if (currStr.length() != 0){
+					tokens[count++] = currStr;
+					currStr = "";
+					i += 4;
+				}
+			}
+			else if (i +3 <= str.length() && state == TokenState.COMMENT && str.substring(i, i+3).equals("-->")){
+				state = TokenState.NONE;
+				i += 4;
+			}
+			if (i < str.length() && state == TokenState.NONE){
+				if (c == '<') {
+					if (currStr.length() > 0) {
 						tokens[count++] = currStr.toString();
 						currStr = "";
+					}
+					inTag = true;
+					currStr += c;
+				}
+				else if (c == '>') {
+					if (inTag) {
+						currStr += c;
+						tokens[count++] = currStr;
+						currStr = "";
+						inTag = false;
+					}
+				}
+				else if (inTag) {
+					currStr += c;
+				}
+				else if (Character.isWhitespace(c)) {
+					if (currStr.length() > 0) {
+						tokens[count++] = currStr.toString();
+						currStr = "";
+					}
+				}
+				else if (Character.isDigit(c) || c == '.') {
+					if (currStr.length() > 0 && 
+						!Character.isDigit(currStr.charAt(currStr.length() - 1)) &&
+						currStr.charAt(currStr.length() - 1) != '.' &&
+						!(currStr.length() == 1 && currStr.charAt(0) == '-')) {
+						tokens[count++] = currStr;
+						currStr = "";
+					}
+					currStr += c;
+				}
+				else if (c == '-') {
+					if (currStr.length() == 0 || 
+						(!Character.isDigit(currStr.charAt(0)) && !Character.isLetter(currStr.charAt(0)))) {
 						currStr += c;
 					}
+					else if (currStr.length() > 0 && Character.isLetter(currStr.charAt(currStr.length() - 1))) {
+							tokens[count++] = currStr.toString();
+							currStr = "";
+							currStr += c;
+						}
+					else {
+						if (currStr.length() > 0) {
+							tokens[count++] = currStr.toString();
+							currStr = "";
+						}
+						tokens[count++] = "" + c;
+					}
+				}
+				else if (Character.isLetter(c)) {
+					if (currStr.length() > 0 && 
+						(!Character.isLetter(currStr.charAt(0)) && currStr.charAt(0) != '-' && !Character.isDigit(currStr.charAt(0)))) {
+						tokens[count++] = currStr;
+						currStr = "";
+					}
+					currStr += c;
+				}
 				else {
-		            if (currStr.length() > 0) {
-		                tokens[count++] = currStr.toString();
-		                currStr = "";
-		            }
-		            tokens[count++] = "" + c;
-		        }
-		    }
-		    else if (Character.isLetter(c)) {
-		        if (currStr.length() > 0 && 
-		            (!Character.isLetter(currStr.charAt(0)) && currStr.charAt(0) != '-' && !Character.isDigit(currStr.charAt(0)))) {
-		            tokens[count++] = currStr;
-		            currStr = "";
-		        }
-		        currStr += c;
-		    }
-		    else {
-		        if (currStr.length() > 0) {
-		            tokens[count++] = currStr;
-		            currStr = "";
-		        }
-		        tokens[count++] = ""+c;
-		    }
+					if (currStr.length() > 0) {
+						tokens[count++] = currStr;
+						currStr = "";
+					}
+					tokens[count++] = ""+c;
+				}
+			}
 		}
 		if (currStr.length() > 0) {
 		    tokens[count++] = currStr;
