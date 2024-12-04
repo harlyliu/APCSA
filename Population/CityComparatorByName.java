6ad41902c3a6ac207d9bd7a0c8605fb9cffde670
harlyliu <99936708+harlyliu@users.noreply.github.com>
2024-12-04 10:01:52 -0800

    Update CityComparatorByName.java

--- Population/CityComparatorByName.java
+++ Population/CityComparatorByName.java
@@ -1,10 +1,9 @@
-/**
- *	Compares names
- *
- *	@author	
- *	@since	
- */
 public class CityComparatorByName implements Comparator<City> {
-	
-	
+	public int compare(City city1, City city2){
+		int val = city1.getName().compareTo(city2.getName());
+		if (val == 0){
+			return city1.getPopulation()-city2.getPopulation();
+		}
+		return val;
+	}
 }
