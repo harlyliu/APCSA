75a545613340d4d06102f230c46b4bd3ddb029b2
harlyliu <99936708+harlyliu@users.noreply.github.com>
2025-03-14 09:49:51 -0700

    Add files via upload

---
+++ GridWorldCode/projects/Activity3/Jumper.java
@@ -0,0 +1,107 @@
+/* 
+ * AP(r) Computer Science GridWorld Case Study:
+ * Copyright(c) 2005-2006 Cay S. Horstmann (http://horstmann.com)
+ *
+ * This code is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * @author Cay Horstmann
+ */
+
+package info.gridworld.actor;
+
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+import info.gridworld.actor.*;
+
+import java.awt.Color;
+
+/**
+ * A <code>Bug</code> is an actor that can move and turn. It drops flowers as
+ * it moves. <br />
+ * The implementation of this class is testable on the AP CS A and AB exams.
+ */
+public class Jumper extends Bug
+{
+    /**
+     * Constructs a red bug.
+     */
+    public Jumper()
+    {
+        setColor(Color.BLUE);
+    }
+
+    /**
+     * Constructs a bug of a given color.
+     * @param bugColor the color for this bug
+     */
+    public Jumper(Color bugColor)
+    {
+        setColor(bugColor);
+    }
+
+    /**
+     * Moves if it can move, turns otherwise.
+     */
+    public void act()
+    {
+        if (canMove())
+            move();
+        else
+            turn();
+    }
+
+    /**
+     * Turns the bug 45 degrees to the right without changing its location.
+     */
+    public void turn()
+    {
+        setDirection(getDirection() + Location.HALF_RIGHT);
+    }
+
+    /**
+     * Moves the bug forward, putting a flower into the location it previously
+     * occupied.
+     */
+    public void jump()
+    {
+        Grid<Actor> gr = getGrid();
+        if (gr == null)
+            return;
+        Location loc = getLocation();
+        Location next = loc.getAdjacentLocation(getDirection());
+        if (gr.isValid(next))
+            moveTo(next);
+        else
+            removeSelfFromGrid();
+        Blossom blossom = new Blossom(getColor());
+        blossom.putSelfInGrid(gr, loc);
+    }
+
+    /**
+     * Tests whether this bug can move forward into a location that is empty or
+     * contains a flower.
+     * @return true if this bug can move.
+     */
+    public boolean canJump()
+    {
+        Grid<Actor> gr = getGrid();
+        if (gr == null)
+            return false;
+        Location loc = getLocation();
+        Location fakeNext = loc.getAdjacentLocation(getDirection());
+        Location next = fakeNext.getAdjacentLocation(getDirection());
+        if (!gr.isValid(next))
+            return false;
+        Actor neighbor = gr.get(next);
+        return (neighbor == null) || (neighbor instanceof Flower) || (neighbor instanceof Blossom);
+        // ok to move into empty location or onto flower
+        // not ok to move onto any other actor
+    }
+}
